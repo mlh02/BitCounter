a26972abd40edee1d16c3eab168b51d6a9122963
Montather Handal <ali.louy2005@gmail.com>
2021-04-26 18:53:22 -0500

    Bit Counter JS Updated

--- BitCounter.js
+++ BitCounter.js
@@ -3,11 +3,12 @@ function bitCounter(n) {
     // Replace /0/ removes all zeros 
     n = n.toString(2).replace(/0/g, '').split('');
     //Counter for how many '1'
-    let counter = 0
-    
-    for(let i = 0; i < n.length; I++)
+    var counter = 0
+    // console.log(n)
+    for(let i = 0; i < n.length; i++)
     {
-    	counter += n[i]
+      counter += parseInt(n[i])
+      // console.log(typeof parseInt(n[i]));
     }
     return counter
 }
